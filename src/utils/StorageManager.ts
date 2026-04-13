e0144cfa1657acef2c571a57ca1b1b11aa65a73a
januszzolcik <158294524+januszzolcik@users.noreply.github.com>
2026-04-13 19:51:10 +0200

    Create src/utils/StorageManager.ts for IndexedDB and cloud save management.

---
+++ src/utils/StorageManager.ts
@@ -0,0 +1,30 @@
+// StorageManager.ts
+
+/**
+ * StorageManager is responsible for managing data storage using IndexedDB
+ * and cloud save functionalities.
+ */
+
+class StorageManager {
+    constructor() {
+        // Initialize IndexedDB and cloud storage configurations
+    }
+
+    async saveToIndexedDB(data) {
+        // Logic to save data to IndexedDB
+    }
+
+    async loadFromIndexedDB(id) {
+        // Logic to load data from IndexedDB
+    }
+
+    async saveToCloud(data) {
+        // Logic to save data to cloud
+    }
+
+    async loadFromCloud(id) {
+        // Logic to load data from cloud
+    }
+}
+
+export default StorageManager;
